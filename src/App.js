d9afa1759aad0e6c57e1de62e43e8d329d4d5c64
unknown <bileljabrane6@gmail.com>
2021-12-06 17:40:51 +0100

    react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Mynew from './newform.js'
+function App() {
+  return (
+    <div className="App">
+    <Mynew></Mynew>
+    </div>
+  );
+}
+
+export default App;
+
